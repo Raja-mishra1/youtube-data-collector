a547660b92cc9e7ab3418ff0d56a449109556731
Raja-mishra1 <rajamishra0702@gmail.com>
2021-07-11 21:52:28 +0530

    [ADD]: added sample task

---
+++ project/core/tasks.py
@@ -0,0 +1,11 @@
+from celery import shared_task
+from celery.utils.log import get_task_logger
+from django.core.management import call_command
+
+
+logger = get_task_logger(__name__)
+
+
+@shared_task
+def sample_task():
+    logger.info("The sample task just ran.")
