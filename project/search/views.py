20035e885acbfc8553a6492abfcad4fb9891f740
Raja-mishra1 <rajamishra0702@gmail.com>
2021-07-11 22:28:07 +0530

    updated search app views to fetch data from DB

--- project/search/views.py
+++ project/search/views.py
@@ -1,11 +1,48 @@
-from django.shortcuts import render
+import boto3
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
 
-# Create your views here.
+
 
 
 def get_video(self,title, *args, **kwargs):
-    pass
+    """[View to show data based on video title]
+
+    Args:
+        title ([str]): [Video title]
+
+    Returns:
+        [json]: [Data from Dynamodb with Video details]
+    """
+    title = title
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Video')
+    try:
+        response = table.get_item(Key={'query': title})
+        return JsonResponse({'data':response})
+    except Exception as e:
+        print(e.response['Error']['Message'])
+    return  Response(status=status.HTTP_404_NOT_FOUND)
+    
+    
 
 
 def get_video_all(self, *args, **kwargs):
-    pass
+    """[View to show all video data from Dynamodb]
+
+    Returns:
+        [json]: [All data from DynamoDb ]
+    """
+    dynamodb = boto3.resource('dynamodb')
+    data = list()
+    table = dynamodb.Table('Video')
+
+    response = table.scan()
+    data = response['Items']
+
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+        
+    return JsonResponse({'data':data})
